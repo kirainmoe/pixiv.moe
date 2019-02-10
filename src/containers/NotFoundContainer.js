8db86253191340ef5337af36417e3f8a67cc9b95
kirainmoe <i@kirainmoe.com>
2019-02-10 15:37:14 +0800

    build gh-pages

--- src/containers/NotFoundContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import Message from '@/components/Message';
-
-const NotFoundContainer = () => <Message />;
-
-export default NotFoundContainer;
--- src/icons/convert.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-for file in apple-touch-icon*.png; do convert $file -background white -flatten $file; done
